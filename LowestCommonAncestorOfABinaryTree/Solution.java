4ccf5d8da979a34c7e26fb0414dca65b48b0ade1
paulanerZ <zhougf01@gmail.com>
2015-12-31 12:52:10 -0500

    LCA

---
+++ LowestCommonAncestorOfABinaryTree/Solution.java
@@ -0,0 +1,41 @@
+package LowestCommonAncestorOfABinaryTree;
+
+/**
+ * Created by Alan on 12/31/2015.
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p) {
+            return p;
+        }
+        if (root == q) {
+            return q;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right == null) {
+            return left;
+        }
+        if (left == null && right != null) {
+            return right;
+        }
+        if (left == null && right == null) {
+            return null;
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
