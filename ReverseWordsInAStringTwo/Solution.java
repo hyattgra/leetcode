b7e9b310c72be8eab710625d020172cf38c79c2a
paulanerZ <zhougf01@gmail.com>
2015-12-31 12:15:16 -0500

    reverse all array and then reverse each word

---
+++ ReverseWordsInAStringTwo/Solution.java
@@ -0,0 +1,29 @@
+package ReverseWordsInAStringTwo;
+
+/**
+ * Created by Alan on 12/31/2015.
+ */
+public class Solution {
+    public void reverseWords(char[] s) {
+
+        reverse(s, 0, s.length - 1);
+        int start = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == ' ') {
+                reverse(s, start, i - 1);
+                start = i + 1;
+            }
+        }
+        reverse(s, start, s.length - 1);
+    }
+
+    private void reverse(char[] s, int start, int end) {
+        while (start < end) {
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
