b54f900a1c2c95b85e49fbde742eddf7ee2c6f88
hyattgra <hyattgra@gmail.com>
2016-01-28 17:00:14 -0500

    Update Solution.java

--- SuperUglyNumber/Solution.java
+++ SuperUglyNumber/Solution.java
@@ -1,11 +1,33 @@
 package SuperUglyNumber;
 
 /**
- * Created by Alan on 1/3/2016.
+ * Created by gzhou on 1/28/16.
  */
 public class Solution {
-
     public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] factor = new int[primes.length];
+        System.arraycopy(primes, 0, factor, 0, primes.length);
 
+        int[] index = new int[primes.length];
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+
+            for (int j = 0; j < factor.length; j++) {
+                min = Math.min(min, factor[j]);
+            }
+
+            ugly[i] = min;
+
+            for (int j = 0; j < factor.length; j++) {
+                if (min == factor[j]) {
+                    factor[j] = primes[j] * ugly[++index[j]];
+                }
+            }
+        }
+
+        return ugly[n - 1];
     }
 }
