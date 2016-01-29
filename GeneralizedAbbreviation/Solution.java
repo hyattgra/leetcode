391d4dbe6d511c621d84b9420b07069226ba60c2
paulanerZ <zhougf01@gmail.com>
2016-01-28 22:16:56 -0500

    dfs

--- GeneralizedAbbreviation/Solution.java
+++ GeneralizedAbbreviation/Solution.java
@@ -1,5 +1,7 @@
 package GeneralizedAbbreviation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,6 +10,28 @@ import java.util.List;
 public class Solution {
 
     public List<String> generateAbbreviations(String word) {
+        StringBuilder sb = new StringBuilder();
+        List<String> result = new ArrayList<>();
+        dfs(result, sb, word.toCharArray(), 0, 0);
+        return result;
+    }
 
+    public void dfs(List<String> result, StringBuilder sb, char[] chars, int i, int num) {
+        int len = sb.length();
+        if (i == chars.length) {
+            if (num != 0) {
+                sb.append(num);
+            }
+            result.add(sb.toString());
+        } else {
+            dfs(result, sb, chars, i + 1, num + 1);
+
+            if (num != 0) {
+                sb.append(num);
+            }
+
+            dfs(result, sb.append(chars[i]), chars, i + 1, 0);
+        }
+        sb.setLength(len);
     }
 }
