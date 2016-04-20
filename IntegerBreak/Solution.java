64db71a314202ab63ba068d9912c2ef77413c859
paulaner <zhougf01@gmail.com>
2016-04-19 22:11:27 -0400

    dp

---
+++ IntegerBreak/Solution.java
@@ -0,0 +1,17 @@
+package IntegerBreak;
+
+/**
+ * Created by Alan on 4/19/2016.
+ */
+public class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(dp[j], j) * Math.max(dp[i - j], i - j));
+            }
+        }
+        return dp[n];
+    }
+}
