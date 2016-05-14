2e56cd5b572332928e98d015f6be35a7208d1d6d
paulaner <zhougf01@gmail.com>
2016-05-14 16:38:23 -0400

    stringbuilder

---
+++ ReverseString/Solution.java
@@ -0,0 +1,10 @@
+package ReverseString;
+
+/**
+ * Created by Alan on 5/14/2016.
+ */
+public class Solution {
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
